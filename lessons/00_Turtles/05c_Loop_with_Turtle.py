9757be91840171212f3166a3a14d03f5d2351494
NaomiB80 <naomizbowers@gmail.com>
2024-09-03 21:43:41 +0000

    Variables and Functions

--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -7,6 +7,14 @@ write a new program that uses a loop to draw a pentagon.
 ( You can cut and past most of it! )
 
 """
+import turtle
+turtle.setup(width=600, height=600)
+
+tina = turtle.Turtle()
+
+tina.shape('turtle')
+tina.speed(2)
+
 for i in range(5):
     tina.forward(90)
     tina.left(72)
--- lessons/00_Turtles/07_Efficient_Turtle.py
+++ lessons/00_Turtles/07_Efficient_Turtle.py
@@ -17,22 +17,34 @@ tina.speed(2)                           # Make the turtle move as fast, but not 
 
 def draw_polygon(sides):
 
-    angle = ... # Calculate angle from number of sides
+    angle = 360/sides # Calculate angle from number of sides
     
-    for i in range(...):                 # Loop through the number of sides
-        ...                              # Move tina forward by the forward distance
-        ...                              # Turn tina left by the left turn
+    for i in range(sides):
+        tina.forward(150)
+        tina.left(angle)                                 # Loop through the number of sides
+                                         # Move tina forward by the forward distance
+                                         # Turn tina left by the left turn
 
+tina.penup
 
-draw_polygon(...)                        # Draw a square
+tina.forward(-400)
+tina.pendown
 
-...                                      # Move tina to another spot on the screen
+draw_polygon(4)                        # Draw a square
 
-draw_polygon(...)                        # Draw a pentagon
+tina.penup
 
-...                                      # Move tina to another spot on the screen
+tina.forward(200)                                      # Move tina to another spot on the screen
+tina.pendown
 
-draw_polygon(...)                        # Draw a hexagon
+draw_polygon(5)                        # Draw a pentagon
+
+tina.penup
+
+tina.forward(200)                                    # Move tina to another spot on the screen
+tina.pendown
+
+draw_polygon(6)                        # Draw a hexagon
 
 
 turtle.exitonclick()                     # Close the window when we click on it
