5a4d6b1ff9344aaf25409bebf8d1816edd73a8df
NaomiB80 <naomizbowers@gmail.com>
2024-09-08 15:47:08 +0000

    LeagueBot makes a hexagon

--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -8,13 +8,19 @@ Write your own turtle program! Here is what your program should do
 4) Draw a hexagon using a loop and variables. 
 
 """
+import turtle
+turtle.setup(width=10, height=10)
 
-import turtle as turtle
+t = turtle.Turtle()
 
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
-t = turtle.Turtle()
+t.pencolor=("blue")
 
-... # Your Code Here
+for i in range(6):
+    t.forward(100)
+    t.left(60)
+
+
