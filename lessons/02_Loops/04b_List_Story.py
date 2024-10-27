faee6abdc12e274ac3523d72b404b6210421765a
NaomiB80 <naomizbowers@gmail.com>
2024-10-27 18:31:25 +0000

    Lists, Tuples and Strings (Indexing and Appending)

--- lessons/02_Loops/04b_List_Story.py
+++ lessons/02_Loops/04b_List_Story.py
@@ -7,10 +7,16 @@ append them to the story
 """
 
 words = ['Once', '👦', 'upon', '🐕', 'park', 'met', 'with', 'a', 'the', 
-    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽.', 'they']
+    'time', 'to', 'who', '🐈', '👧', 'and', 'went', 'had', 'play', '⚽', 'they']
 
 story = []
 
+story = story + words[:1] + words[2:3] + words[7:8] + words[9:10] + [','] + words[7:8]
+story = story + words[-7:-6] + words[-5:-4] + words[6:7] + ['her'] + words[-8:-7] 
+story = story + words[10:11] + words[8:9] + words[4:5] + words[10:11] + words[-3:-2] 
+story = story + words[6:7] + words[7:8] + words[-2:-1] + words[-6:-5] + words[5:6]
+story = story + words[7:8] + words [1:2] + words[6:7] + words[7:8] + words[3:4] + [',']
+story = story + words[-6:-5] + words[-1:] + ['played'] + ['together'] + ['.']
 # Create a story using the words in the list
 
 # Display the story to the user
