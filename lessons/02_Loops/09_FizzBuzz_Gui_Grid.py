bdb47d8c5be41416d746fa6a579f4fd0df10bf41
NaomiB80 <naomizbowers@gmail.com>
2024-11-05 22:43:58 +0000

    10x10 fizz buzz grid

--- lessons/02_Loops/09_FizzBuzz_Gui_Grid.py
+++ lessons/02_Loops/09_FizzBuzz_Gui_Grid.py
@@ -33,11 +33,22 @@ app = App("Numbers Grid", layout="grid")
 
 # Create a 10x10 grid using nested loops
 # Or you can use a single loop and calculate the row and column
+for i in range(10):
+    for j in range(10):
+        print(f"{j+i*10+1}", end= ' ')
+    print('')
 
 # In the loop, calculate or increment the number
 
 # Use % determing the display, using fizzbuzz rules
-
+if i % 5 == 0:
+    print('🦡')
+elif i % 3 == 0:
+    print('🍄')
+elif i % 15 == 0:
+    print('🐍')
+else:
+    print(i)
 # If you are displaying a number, calculate the sum of the digits and determine the color
 
 # Call Text(app, text='...', grid=[col, row], color=...) to display something. 
